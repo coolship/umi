55596f53dfa71fed6e11a512d08d3b04bd94a0a0
Ben Holmes <ben@coolship.io>
2018-05-05 15:50:14 -0400

    toy model for parallelizable demultiplex

--- demultiplex.py
+++ demultiplex.py
@@ -18,13 +18,20 @@ logger = logging.getLogger('root')
 #args['index1'] = os.path.join(base, 'Undetermined_S0_L001_I1_001.fastq.gz')
 #args['index2'] = os.path.join(base, 'Undetermined_S0_L001_I2_001.fastq.gz')
 
-def fq(file):
+def fq(file, start=0, max_count=-1):
+    count=0
     if re.search('.gz$', file):
         fastq = gzip.open(file, 'rb')
     else:
         fastq = open(file, 'r')
     with fastq as f:
+        if start>0:
+            junk = f.readlines(4*start)
+
         while True:
+            if max_count >0 and count >max_count: break
+            count+=1
+
             l1 = f.readline()
             if not l1:
                 break
@@ -42,6 +49,32 @@ def get_sample_id(i1, i2, sample_names):
         return sample_names[sample_barcode]
     else:
         return sample_barcode
+
+
+def read_core(read1,read2,index1,index2,sample_names,start_record,stride):
+        r1s = [fq(read1,start=start_record,count=stride)]
+        r2s = [fq(read2,start=start_record,count=stride)]
+        i1s = [fq(index1,start=start_record,count=stride)]
+        i2s = [fq(index2,start=start_record,count=stride)]
+
+        if len(r1s) == 0:
+            return None
+
+        ids = [get_sample_id(i1,i2,sample_names) for i1, i2 in i1s[start_record:end_record], i2s[start_record:end_record]]
+
+        keys = set(ids)
+        r1_map = dict([(k:[]) for k in keys)
+        r2_map = dict([(k:[]) for k in keys)
+        i1_map = dict([(k:[]) for k in keys)
+        i2_map = dict([(k:[]) for k in keys)
+
+        for i,e in ids:
+            r1_map[ids[i]] = r1s[i]
+            r2_map[ids[i]] = r2s[i]
+            i1_map[ids[i]] = i1s[i]
+            i2_map[ids[i]] = i2s[i]
+
+        return r1_map, r2_map, i1_map, i2_map
 
 
 def demultiplex(read1, read2, index1, index2, sample_barcodes, out_dir, min_reads=10000):
@@ -74,30 +107,36 @@ def demultiplex(read1, read2, index1, index2, sample_barcodes, out_dir, min_read
     #it = itertools.izip(fq(args['read1']), fq(args['read2']), fq(args['index1']), fq(args['index2']))
     #for r1,r2,i1,i2 in itertools.islice(it, 0, 100):
     start = time.time()
-    for r1,r2,i1,i2 in itertools.izip(fq(read1), fq(read2), fq(index1), fq(index2)):
-        total_count += 1
-        if total_count % 1000000 == 0:
-            logger.info("Processed %d reads in %.1f minutes.", total_count, (time.time()-start)/60)
-        sample_id = get_sample_id(i1, i2, sample_names)
 
-        # Increment read count and create output buffers if this is a new sample barcode
-        if not count.has_key(sample_id):
-            count[sample_id] = 0
-            buffer_r1[sample_id] = []
-            buffer_r2[sample_id] = []
-            buffer_i1[sample_id] = []
-            buffer_i2[sample_id] = []
-        count[sample_id] += 1
+    cores = 10
+    stride = 10000
+    total_count = 0
 
-        # Write the reads to file or to sample-specific output buffers if we haven't
-        # reached min_reads
-        # The buffers are used to avoid writing fastqs for sample barcodes with very few reads.
-        if count[sample_id] < min_reads:
-            buffer_r1[sample_id].append(r1)
-            buffer_r2[sample_id].append(r2)
-            buffer_i1[sample_id].append(i1)
-            buffer_i2[sample_id].append(i2)
-        elif count[sample_id] == min_reads:
+    all_r1s = {}
+    all_r2s = {}
+    all_i1s = {}
+    all_i2s = {}
+
+    while True
+        for i,c in range(cores):
+
+            start_record = i*stride
+            end_record=(i+1)*stride
+            out = read_core(read1,read2,index1,index2,sample_names,start_record,stride)
+            if out == None:
+                break
+            else:
+                total_count += len(out[0])
+
+            all_r1s.extend(out[0])
+            all_r2s.extend(out[1])
+            all_i1s.extend(out[2])
+            all_i2s.extend(out[3])
+        break
+
+    for sample_id in all_r1s.keys():
+
+        if len(all_r1s[k]) >= min_reads:
             outfiles_r1[sample_id] = open(os.path.join(out_dir, '%s.r1.fastq' % sample_id), 'w')
             outfiles_r2[sample_id] = open(os.path.join(out_dir, '%s.r2.fastq' % sample_id), 'w')
             outfiles_i1[sample_id] = open(os.path.join(out_dir, '%s.i1.fastq' % sample_id), 'w')
@@ -111,19 +150,11 @@ def demultiplex(read1, read2, index1, index2, sample_barcodes, out_dir, min_read
                 outfiles_i1[sample_id].write(''.join(record))
             for record in buffer_i2[sample_id] + i2:
                 outfiles_i2[sample_id].write(''.join(record))
-            del buffer_r1[sample_id]
-            del buffer_r2[sample_id]
-            del buffer_i1[sample_id]
-            del buffer_i2[sample_id]
-        else:
-            for line in r1:
-                print (line, file=outfiles_r1[sample_id], end="")
-            for line in r2:
-                print (line, file=outfiles_r2[sample_id], end="")
-            for line in i1:
-                print (line, file=outfiles_i1[sample_id], end="")
-            for line in i2:
-                print (line, file=outfiles_i2[sample_id], end="")
+
+            del all_r1s[sample_id]
+            del all_r2s[sample_id]
+            del all_i1s[sample_id]
+            del all_i2s[sample_id]
 
     # Write remaining buffered reads to a single fastq.
     # (These reads correspond to barcodes that were seen less than min_reads times)
@@ -131,14 +162,14 @@ def demultiplex(read1, read2, index1, index2, sample_barcodes, out_dir, min_read
     undetermined_r2 = open(os.path.join(out_dir, 'undetermined.r2.fastq'), 'w')
     undetermined_i1 = open(os.path.join(out_dir, 'undetermined.i1.fastq'), 'w')
     undetermined_i2 = open(os.path.join(out_dir, 'undetermined.i2.fastq'), 'w')
-    for sample_id in buffer_r1.keys():
-        for record in buffer_r1[sample_id]:
+    for sample_id in all_r1s.keys():
+        for record in all_r1s[sample_id]:
             undetermined_r1.write(''.join(record))
-        for record in buffer_r2[sample_id]:
+        for record in all_r2s[sample_id]:
             undetermined_r2.write(''.join(record))
-        for record in buffer_i1[sample_id]:
+        for record in all_i1s[sample_id]:
             undetermined_i1.write(''.join(record))
-        for record in buffer_i2[sample_id]:
+        for record in all_i2s[sample_id]:
             undetermined_i2.write(''.join(record))
 
     # Close files
@@ -152,8 +183,8 @@ def demultiplex(read1, read2, index1, index2, sample_barcodes, out_dir, min_read
     undetermined_i1.close()
     undetermined_i2.close()
 
-    num_fastqs = len([v for k,v in count.iteritems() if v>=min_reads])
-    logger.info('Wrote FASTQs for the %d sample barcodes out of %d with at least %d reads.', num_fastqs, len(count), min_reads)
+
+    logger.info('Wrote FASTQs for the %d sample barcodes out of %d with at least %d reads.', len(outfiles_r1), total_count, min_reads)
 
 def main():
     parser = argparse.ArgumentParser()
